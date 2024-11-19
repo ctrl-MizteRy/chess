3677d9914ba9eae39407132140bdfd3644ef5bda
Khang <le.h.khang97@gmail.com>
2024-11-18 18:27:40 -0600

    fixing the bugs with pawn moves

--- src/main/java/game/chessgame/ChessPieces.java
+++ src/main/java/game/chessgame/ChessPieces.java
@@ -4,20 +4,24 @@ class ChessPieces {
     private int index = 0;
     protected int[][] Pawn(int row, int col, String player, String[][] top, String[][] bottom, String pieceColor) {
         int[][] moves = new int[4][2];
-        int index = 0;
+        index = 0;
         if (player.equals(pieceColor) && row == 6){
-            moves[index++] = new int[]{row - 1, col};
-            moves[index++] = new int[] {row -2 , col};
+            if (bottom[row - 1][col].isEmpty() && top[row - 1][col].isEmpty()) {
+                moves[index++] = new int[]{row - 1, col};
+                if (bottom[row-2][col].isEmpty() && top[row-2][col].isEmpty()) {
+                    moves[index++] = new int[]{row - 2, col};
+                }
+            }
         }
         else if (player.equals(pieceColor) && row != 0){
-            if (top[row-1][col].isEmpty()) {
+            if (top[row-1][col].isEmpty() && bottom[row-1][col].isEmpty()) {
                 moves[index++] = new int[]{row - 1, col};
             }
         }
         else if (!player.equals(pieceColor) && row == 1){
-            if (bottom[row+1][col].isEmpty()) {
+            if (bottom[row+1][col].isEmpty() && top[row+1][col].isEmpty()) {
                 moves[index++] = new int[]{row + 1, col};
-                if (bottom[row+2][col].isEmpty()) {
+                if (bottom[row+2][col].isEmpty() && top[row+2][col].isEmpty()) {
                     moves[index++] = new int[]{row + 2, col};
                 }
             }
@@ -28,24 +32,24 @@ class ChessPieces {
             }
         }
         if (player.equals(pieceColor)){
-            if (!top[row-1][col-1].isEmpty()){
+            if (col - 1 >= 0  && !top[row-1][col-1].isEmpty()){
                 if (!top[row-1][col+1].isEmpty()){
                     moves[index++] = new int[] {row-1, col+1};
                 }
                 moves[index++] = new int[] {row-1, col-1};
             }
-            else if (!top[row-1][col+1].isEmpty()){
+            else if (col + 1 <= 7 && !top[row-1][col+1].isEmpty()){
                 moves[index++] =  new int[] {row-1, col+1};
             }
         }
         else{
-            if (!bottom[row+1][col-1].isEmpty()){
+            if (col - 1 >= 0 && !bottom[row+1][col-1].isEmpty()){
                 if (!bottom[row+1][col+1].isEmpty()){
                     moves[index++] = new int[] {row+1, col+1};
                 }
                 moves[index++] = new int[] {row+1, col-1};
             }
-            else if (!bottom[row+1][col+1].isEmpty()){
+            else if (col + 1 <= 7 && !bottom[row+1][col+1].isEmpty()){
                 moves[index++] = new int[] {row+1, col+1};
             }
         }
